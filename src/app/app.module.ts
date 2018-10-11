186d2f27c396fa0f4fd71c4d9ca8bb72b1d28c5f
julie-nguyen-projects <julie_nguyen@live.fr>
2018-10-11 15:51:47 +0200

    feat (login): authentication service, login page and user model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {AuthenticationService} from './services-api/AuthenticationService';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
   ],
-  providers: [],
+  providers: [AuthenticationService, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
