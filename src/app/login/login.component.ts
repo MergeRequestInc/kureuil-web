8023dedb31bd0225fbd8386ecdd62708cda930f1
Jerome Dobbels <jerome.dobbels@epitech.eu>
2018-12-12 12:09:10 +0100

    feat(NavBar): Adding the navbar without any style

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -36,6 +36,7 @@ export class LoginComponent implements OnInit {
     console.log(this.user);
     this.authenticationService.login(this.user).subscribe((userConnected) => {
       this.user = userConnected;
+      localStorage.setItem('isLoggedIn', 'true');
     });
   }
 }
