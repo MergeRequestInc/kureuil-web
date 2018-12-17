119b40c2c81fbbf21e77ac916899f2f4d1fa53a1
julie-nguyen-projects <julie_nguyen@live.fr>
2018-12-17 11:47:28 +0100

    refactor: components register and login moved into account directory

---
+++ src/app/account/register/register.component.ts
@@ -0,0 +1,35 @@
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {MessageService} from 'primeng/api';
+import {User} from '../../model/user';
+import {UserService} from '../../services-api/user.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  model: User = new User;
+  loading = false;
+
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private messageService: MessageService) { }
+
+  register() {
+    this.loading = true;
+    this.userService.create(this.model)
+      .subscribe(
+        () => {
+          this.messageService.add({severity: 'success', summary: 'Success', detail: 'Registration successful'});
+          this.router.navigate(['login']);
+        },
+        () => {
+          this.messageService.add({severity: 'error', summary: 'Error',
+            detail: 'Registration impossible. Please contact an administrator'});
+          this.loading = false;
+        });
+  }
+}
