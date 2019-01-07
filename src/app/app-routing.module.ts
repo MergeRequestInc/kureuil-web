e5ef073572665dd67d590b6e07fa8fdea9cab4fa
julie-nguyen-projects <julie_nguyen@live.fr>
2019-01-07 21:33:12 +0100

    feat(routing): authorization to access home component implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './account/register/register.component';
 import {ForgottenPwdComponent} from './account/forgotten-pwd/forgotten-pwd.component';
 import {ChangePwdComponent} from './account/change-pwd/change-pwd.component';
+import {AuthorizationOnlineService} from './services-api/authorization-online.service';
 
-
+/**
+ * Routing of the application
+ */
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', component: HomeComponent, canActivate: [AuthorizationOnlineService]},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'forgotPwd', component: ForgottenPwdComponent},
