c1c4e36438fc00641dd2bd7487faf330ac7410da
julie-nguyen-projects <julie_nguyen@live.fr>
2019-01-07 21:17:45 +0100

    feat(routing): authorization for routing begun

---
+++ src/app/services-api/authorization-online.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthenticationService} from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizationOnlineService implements CanActivate {
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authenticationService.isLogin()) {
+      return true;
+    } else {
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+}
