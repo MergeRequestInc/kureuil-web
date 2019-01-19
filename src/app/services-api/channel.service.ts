c2f9192db3ad8db4c0377d689fc4c1283ac74a87
julie-nguyen-projects <julie_nguyen@live.fr>
2019-01-19 10:31:29 +0100

    style(channel service): fixed lint warnings

--- src/app/services-api/channel.service.ts
+++ src/app/services-api/channel.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Channel} from "../model/channel";
-import {SERVER_API_URL} from "../services-common/constants/app.constants";
+import {HttpClient} from '@angular/common/http';
+import {Channel} from '../model/channel';
+import {SERVER_API_URL} from '../services-common/constants/app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -10,24 +10,20 @@ export class ChannelService {
 
   constructor(private http: HttpClient) { }
 
-  findAll()
-  {
+  findAll() {
     return this.http.get<Channel[]>( SERVER_API_URL + 'channels');
   }
 
-  create(channel: Channel)
-  {
+  create(channel: Channel) {
     return this.http.post(SERVER_API_URL + 'channels', channel);
   }
 
-  update (channel: Channel)
-  {
+  update (channel: Channel) {
     return this.http.post(SERVER_API_URL + 'channels', channel);
   }
 
 
-  delete (id: number)
-  {
+  delete (id: number) {
     return this.http.delete(SERVER_API_URL + 'channels/' + id);
   }
 
