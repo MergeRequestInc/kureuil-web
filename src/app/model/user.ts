d6d305c4e537620154bfce965ee3ea1277a70927
julie-nguyen-projects <julie_nguyen@live.fr>
2019-01-19 11:30:28 +0100

    feat(model): add BuilderDtoJsonAbstract to facilitate eventual conversions

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,8 +1,12 @@
-export class User {
+import {BuilderDtoJsonAbstract} from './builderDtoJsonAbstract';
+
+export class User extends BuilderDtoJsonAbstract {
   constructor(
     public id?: number,
     public name?: string,
     public email?: string,
     public password?: string,
-  ) {}
+  ) {
+    super();
+  }
 }
