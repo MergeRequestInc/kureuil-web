af49994281473e77448c042e659f2c6b6190e332
julie-nguyen-projects <julie_nguyen@live.fr>
2019-01-28 11:22:10 +0100

    style(home): indentation edited

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,57 +25,57 @@ export class HomeComponent implements OnInit {
     private linkService: LinkService
   ) { }
 
-    ngOnInit() {
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  ngOnInit() {
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
 
-        // TODO load links of the first channel
-        this.links = [
-          new Link(1, 'http://www.google.fr'),
-          new Link(2, 'http://www.youtube.fr'),
-          new Link(3, 'http://www.impot.gouv.fr'),
-          new Link(4, 'http://www.angular.io'),
-        ];
-    }
+    // TODO load links of the first channel
+    this.links = [
+      new Link(1, 'http://www.google.fr'),
+      new Link(2, 'http://www.youtube.fr'),
+      new Link(3, 'http://www.impot.gouv.fr'),
+      new Link(4, 'http://www.angular.io'),
+    ];
+  }
 
   /**
    * Redirect to the link creation page
    */
   newLink() {
-      this.router.navigate(['/newLink']);
-    }
+    this.router.navigate(['/newLink']);
+  }
 
   /**
    * Update the query saved in the component and reload the assoaciated links
    * @param query : query to consider
    */
   loadQuery(query: string) {
-      this.query = query;
-      this.loadLinks();
-    }
+    this.query = query;
+    this.loadLinks();
+  }
 
   /**
    * Load the links linked to a query
    */
   loadLinks() {
-      this.linkService.getByQuery(this.query).subscribe( links => this.links = links);
-    }
+    this.linkService.getByQuery(this.query).subscribe( links => this.links = links);
+  }
 
   /**
    * Update a link
    * @param linkId : link's id to update
    */
   updateLink(linkId) {
-      this.router.navigate(['/editLink', linkId]);
-    }
+    this.router.navigate(['/editLink', linkId]);
+  }
 
   /**
    * Delete a link
    * @param link : link to delete
    */
   deleteLink(link: Link) {
-      this.linkService.delete(link.id).subscribe(() => this.loadLinks());
-    }
+    this.linkService.delete(link.id).subscribe(() => this.loadLinks());
+  }
 
   /**
    * Load Links linked to the channel clicked in left menu
